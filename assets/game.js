b873a6b94e14e8b5b8c0501b60881a3b7aa31d30
Lucas Santos <108681755+lucas-ssantos@users.noreply.github.com>
2023-07-14 18:46:02 -0300

    Initial files

---
+++ assets/game.js
@@ -0,0 +1,30 @@
+var myGamePlayer;
+
+function startGame() {
+    myGameArea.start();
+	myGamePlayer = new component(30, 30, "red", 10, 120);
+}
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  ctx = myGameArea.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		this.interval = setInterval(updateGameArea, 20);
+    },
+	clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
---
+++ assets/main.js
@@ -0,0 +1,6 @@
+function EntrarSite()
+{
+	var site = document.getElementById("sites");
+	
+	window.location.href = "/"+site+"/index.html";
+}
